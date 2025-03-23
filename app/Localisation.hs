b5847dc0e98c735255965fa41852de15b49b981e
Sasaki Touma <netst915@gmail.com>
2025-03-23 14:38:22 +0900

    update css

--- app/Localisation.hs
+++ app/Localisation.hs
@@ -3,6 +3,11 @@ module Localisation
     , htmlLanguageCode
     , appTitle
     , editButtonLabel
+    , finishedLabel
+    , nameLabel
+    , amountLabel
+    , importanceLabel
+    , notesLabel
     ) where
 
 import           Data.Text   (Text)
@@ -29,3 +34,23 @@ appTitle Japanese = "買い物リスト"
 editButtonLabel :: Language -> Text
 editButtonLabel English  = "Edit"
 editButtonLabel Japanese = "編集"
+
+finishedLabel :: Language -> Text
+finishedLabel English  = "Finished"
+finishedLabel Japanese = "完了"
+
+nameLabel :: Language -> Text
+nameLabel English  = "Name"
+nameLabel Japanese = "名称"
+
+amountLabel :: Language -> Text
+amountLabel English  = "Amount"
+amountLabel Japanese = "個数"
+
+importanceLabel :: Language -> Text
+importanceLabel English  = "Importance"
+importanceLabel Japanese = "重要性"
+
+notesLabel :: Language -> Text
+notesLabel English  = "Notes"
+notesLabel Japanese = "備考"

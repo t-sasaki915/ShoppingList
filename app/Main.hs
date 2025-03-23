fc1246a645e72d869fa22fdb7166e4358ceab5d7
Sasaki Touma <netst915@gmail.com>
2025-03-23 09:39:57 +0900

    use warp

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main (main) where
 
-import Lib
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai as Wai
+import qualified Network.HTTP.Types as HTypes
 
 main :: IO ()
-main = someFunc
+main = Warp.run 8080 webServer
+
+webServer :: Wai.Application
+webServer req send = send $ Wai.responseBuilder HTypes.status200 [] "aaaa"

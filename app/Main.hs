9c14b4a5cdd668b16dddd27f7760c26006a5afd1
Toma Sasaki <netst915@gmail.com>
2025-06-06 17:59:11 +0900

    install yesod

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,47 @@
+{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 module Main (main) where
 
-import           AppConfig.Loader     (loadConfig)
-import           Data.Version         (showVersion)
-import           Database.Initialiser (initialiseDatabase)
-import           Paths_ShoppingList   (version)
-import           Text.Printf          (printf)
+import           Data.Functor           ((<&>))
+import           Data.Version           (showVersion)
+import           Database.SQLite.Simple (Connection)
+import           Lucid
+import           Paths_ShoppingList     (version)
+import           Text.Printf            (printf)
+import           Yesod                  hiding (loadConfig)
+import           Yesod.Lucid            (LucidHtml, lucid)
+
+import           AppConfig              (AppConfig (..))
+import           AppConfig.Loader       (loadConfig)
+import           Database.Initialiser   (initialiseDatabase)
+import           Localisation
+
+data ShoppingList = ShoppingList
+    { interfaceLanguage  :: Language
+    , databaseConnection :: Connection
+    }
+
+mkYesod "ShoppingList" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod ShoppingList
+
+getHomeR :: Handler LucidHtml
+getHomeR = do
+    interfaceLang <- getYesod <&> interfaceLanguage
+
+    lucid $ const $ do
+        doctype_
+        html_ [lang_ (htmlLanguageCode interfaceLang)] $ do
+            head_ [] $ do
+                meta_ [charset_ "utf-8"]
+                meta_ [name_ "viewport", content_ "width=device-width,initial-scale=1"]
+                link_ [rel_ "stylesheet", href_ "style.css"]
+                title_ (localiseHtml AppTitle interfaceLang)
 
 main :: IO ()
 main = do
@@ -13,4 +50,8 @@ main = do
     appConfig <- loadConfig "config.yaml"
     database  <- initialiseDatabase "database.db"
 
-    return ()
+    warp (serverPort appConfig) $
+        ShoppingList
+            { interfaceLanguage  = webInterfaceLanguage appConfig
+            , databaseConnection = database
+            }
